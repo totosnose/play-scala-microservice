c14b1fece518887353e28de426cb57ed48191341
nathandope <a.p.alksv@gmail.com>
2019-05-06 19:15:25 +0400

    some little changes in modules

--- app/modules/UserModule.scala
+++ app/modules/UserModule.scala
@@ -1,17 +1,14 @@
 package modules
 
-import play.api.inject.{Binding, Module}
-import play.api.{Configuration, Environment}
+import com.google.inject.AbstractModule
 import repositories.{UserRepository, UserRepositoryImpl}
 import services.{UserService, UserServiceImpl}
 
 /** https://www.playframework.com/documentation/2.7.x/ScalaDependencyInjection#Binding-annotations **/
-// todo abstract module
-class UserModule extends Module {
-  def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
-    Seq(
-      bind[UserService].to[UserServiceImpl],
-      bind[UserRepository].to[UserRepositoryImpl]
-    )
+
+class UserModule extends AbstractModule {
+  override def configure(): Unit = {
+    bind(classOf[UserService]).to(classOf[UserServiceImpl])
+    bind(classOf[UserRepository]).to(classOf[UserRepositoryImpl])
   }
 }
